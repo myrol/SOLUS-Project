08e8004500654071809051b68af89f5ed320e6b7
myrol <posthomerik@gmail.com>
2022-05-31 16:37:31 +0200

    Added two small puzzles in tutorial
    Added opening doors

--- SOLUS/Assets/Scripts/Objects/Door.cs
+++ SOLUS/Assets/Scripts/Objects/Door.cs
@@ -14,9 +14,8 @@ public class Door : MonoBehaviour
     {
         if (canOpen == false) return;
 
-        if (transform.rotation.y < 0.7071)
+        if (transform.localRotation.y < 0.7071)
         {
-            Debug.Log(transform.rotation.y);
             transform.Rotate(new Vector3(0, 1, 0));
         }
     }
