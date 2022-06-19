bf43057770ce500257368507264f2171f3a480da
xEspeox <103063971+xEspeox@users.noreply.github.com>
2022-06-19 19:18:04 +0200

    Room 2 Scripts added
    + LiveFeed Script for rendering Live Cameras
    + Some Puzzles added for Room 2

---
+++ SOLUS/Assets/Scripts/Objects/LiveFeed/LiveFeed.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LiveFeed : Interactable
+{
+
+    [SerializeField] public UnityEvent eve;
+    // Start is called before the first frame update
+    protected override void Interact()
+    {
+        activate();
+    }
+
+    private void activate()
+    {
+        eve.Invoke();
+    }
+}
+
