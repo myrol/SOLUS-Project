fa6d0128cb831a60f0f80463d601e7dfa94512d6
erikrohr <posthomerik@gmail.com>
2022-06-19 20:18:16 +0200

    Test Lighting

--- SOLUS/Assets/Scripts/Objects/Door.cs
+++ SOLUS/Assets/Scripts/Objects/Door.cs
@@ -7,16 +7,24 @@ public class Door : MonoBehaviour
 {
     [SerializeField] private bool openOtherWay = false;
 
+    private Vector3 start;
+    private Vector3 target;
+
     private float animationDuration = 3f;
+
+    private void Awake()
+    {
+        start = transform.localEulerAngles;
+        target = (openOtherWay) ? new Vector3(0f, start.y + 90f, 0f) : new Vector3(0f, start.y - 90f, 0f);
+    }
+
     public void open()
     {
-        if (!openOtherWay)
-        {
-            transform.DOLocalRotate(new Vector3(0f, transform.localEulerAngles.y + 90f, 0f), animationDuration);
-        }
-        else
-        {
-            transform.DOLocalRotate(new Vector3(0f, transform.localEulerAngles.y - 90f, 0f), animationDuration);
-        }
+        transform.DOLocalRotate(target, animationDuration);
+    }
+
+    public void close()
+    {
+        transform.DOLocalRotate(start, animationDuration);
     }
 }
