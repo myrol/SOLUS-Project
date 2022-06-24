470634f646eae5b1657c8badef6cad14106d93a4
JohnFCreep <79384133+JohnFCreep@users.noreply.github.com>
2022-06-24 16:58:22 +0200

    announcer meltdown

--- SOLUS/Assets/Scripts/reaktorMeltdown.cs
+++ SOLUS/Assets/Scripts/reaktorMeltdown.cs
@@ -6,6 +6,9 @@ public class reaktorMeltdown : MonoBehaviour
 {
     public GameObject light1, light2, light3, light4, seekers, screen1, screen2, reactor_screen1, reactor_screen2, reactor_screen3, reactor_screen4;
     public AudioClip melting, flink;
+    private bool done = false;
+    float originalIntensity;
+    Color32 originalColor;
 
     private void Start()
     {
@@ -17,7 +20,11 @@ public class reaktorMeltdown : MonoBehaviour
 
     public void startMeltdownRrountine()
     {
-        StartCoroutine(meltdown());
+        originalColor = light1.GetComponent<Light>().color;
+        originalIntensity = light1.GetComponent<Light>().intensity;
+        if (!done)
+            StartCoroutine(meltdown());
+        done = true;
     }
 
     public IEnumerator meltdown()
@@ -29,16 +36,36 @@ public class reaktorMeltdown : MonoBehaviour
         StartCoroutine(meltdownLight(light3));
         StartCoroutine(meltdownLight(light4));
         yield return new WaitForSeconds(3);
+        while (light1.GetComponent<Light>().intensity > 0)
+        {
+            light1.GetComponent<Light>().intensity -= Time.deltaTime * 50;
+            light2.GetComponent<Light>().intensity -= Time.deltaTime * 50;
+            light3.GetComponent<Light>().intensity -= Time.deltaTime * 50;
+            light4.GetComponent<Light>().intensity -= Time.deltaTime * 50;
+            yield return null;
+        }
         reactor_screen1.SetActive(true);
         reactor_screen2.SetActive(true);
         reactor_screen3.SetActive(true);
         reactor_screen4.SetActive(true);
+        seekers.SetActive(true);
+        light1.GetComponent<Light>().color = originalColor;
+        light2.GetComponent<Light>().color = originalColor;
+        light3.GetComponent<Light>().color = originalColor;
+        light4.GetComponent<Light>().color = originalColor;
+        while (light1.GetComponent<Light>().intensity < originalIntensity)
+        {
+            light1.GetComponent<Light>().intensity += Time.deltaTime * 50;
+            light2.GetComponent<Light>().intensity += Time.deltaTime * 50;
+            light3.GetComponent<Light>().intensity += Time.deltaTime * 50;
+            light4.GetComponent<Light>().intensity += Time.deltaTime * 50;
+            yield return null;
+        }
     }
 
     public IEnumerator meltdownLight(GameObject lightObject)
     {
         AudioSource source = gameObject.GetComponent<AudioSource>();
-        float originalIntensity = lightObject.GetComponent<Light>().intensity;
         source.PlayOneShot(flink);
         while (lightObject.GetComponent<Light>().intensity > 0)
         {
